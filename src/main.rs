eaddb981cdb74d25b9291dfffb39f7745b5732a4
equal-l2 <eng.equall2@gmail.com>
2019-07-21 00:46:32 +0900

    Impl cache

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,42 @@
-use actix_web::web;
-use once_cell::sync::OnceCell;
-use serde::Deserialize;
-use std::collections::BinaryHeap;
-use tag_geotag::*;
-
 mod load;
+mod structs;
+
+use actix_web::web;
+use once_cell::sync::{Lazy, OnceCell};
+use parking_lot::RwLock;
+use std::collections::{BinaryHeap, VecDeque};
+use structs::*;
 
 type HashMap<K, V> = load::HashMap<K, V>;
 
 static TAGS: OnceCell<HashMap<String, Vec<u64>>> = OnceCell::new();
-static GEOTAGS: OnceCell<HashMap<u64, GeoTag>> = OnceCell::new();
+static GEOTAGS: OnceCell<HashMap<u64, tag_geotag::GeoTag>> = OnceCell::new();
+static CACHE: Lazy<RwLock<VecDeque<CacheWrap>>> =
+    Lazy::new(|| RwLock::new(VecDeque::with_capacity(CACHE_LENGTH)));
 const ENTRY_COUNT: usize = 100;
 
 // This SHOULD be equal or more than ENTRY_COUNT
 const STRATEGY_BORDER: usize = 5000;
 
-#[derive(Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct QueryWrap {
-    tag: String,
-}
+const CACHE_LENGTH: usize = 100;
 
-struct DataPair<'a> {
-    id: u64,
-    geotag: &'a GeoTag,
-}
-
-impl<'a> Ord for DataPair<'a> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.geotag.time.cmp(&other.geotag.time)
-    }
-}
-
-impl<'a> PartialOrd for DataPair<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.geotag.time.partial_cmp(&other.geotag.time)
-    }
-}
-
-impl<'a> Eq for DataPair<'a> {}
-
-impl<'a> PartialEq for DataPair<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.geotag.time == other.geotag.time
-    }
+fn get_cache(tag: &str) -> Option<String> {
+    CACHE
+        .read()
+        .iter()
+        .find(|&e| &e.tag == tag)
+        .map(|cw| cw.content.clone())
 }
 
 fn query(q: web::Query<QueryWrap>) -> String {
-    if let Some(i) = TAGS.get().unwrap().get(&q.tag) {
-        if i.len() < STRATEGY_BORDER {
+    let tag = &q.tag;
+
+    if let Some(i) = get_cache(tag) {
+        return i;
+    }
+
+    if let Some(i) = TAGS.get().unwrap().get(tag) {
+        let s = if i.len() < STRATEGY_BORDER {
             // fetch all data, sort them, and take the needed elements
             let mut v = i
                 .into_iter()
@@ -63,8 +52,7 @@ fn query(q: web::Query<QueryWrap>) -> String {
                 .collect::<Vec<_>>()
         } else {
             // fetch data, put it into the heap, then take all and sort them
-            let mut v = i
-                .into_iter()
+            i.into_iter()
                 .map(|id| DataPair {
                     id: *id,
                     geotag: &GEOTAGS.get().unwrap()[id],
@@ -82,21 +70,24 @@ fn query(q: web::Query<QueryWrap>) -> String {
                         heap
                     },
                 )
+                .into_sorted_vec()
                 .into_iter()
-                .map(|t| t.0)
-                .collect::<Vec<_>>();
-            v.sort_unstable_by(|a, b| a.cmp(&b).reverse());
-            v.into_iter()
-                .map(|t| t.geotag.to_csv_row(t.id))
+                .map(|t| t.0.geotag.to_csv_row(t.0.id))
                 .collect::<Vec<_>>()
         }
-        .join("")
-    } else {
-        "".into()
+        .join("");
+        CACHE.write().push_back(CacheWrap {
+            tag: tag.clone(),
+            content: s.clone(),
+        });
+        return s;
     }
+
+    "".into()
 }
 
 fn main() {
+    // parse args
     let mut args = std::env::args().skip(1);
     let _ = load::BASE_DIR.set(args.next().unwrap());
     let worker_num = {
@@ -118,6 +109,7 @@ fn main() {
         }
     };
 
+    // initialize global variables
     println!("Now loading... (Wait patiently)");
     let now = std::time::Instant::now();
     let h1 = std::thread::spawn(|| TAGS.set(load::load_tags("tag_pp.csv")));
@@ -131,6 +123,7 @@ fn main() {
         (now.elapsed().as_millis() as f64) / 1000f64
     );
 
+    // start the web server
     let bind_addr = "0.0.0.0:8080";
     println!("Server listening at '{}'", bind_addr);
     let _ = actix_web::HttpServer::new(|| {
